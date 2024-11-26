1d24c725559f936443dc2c9c16c4e0de08aff3c6
ori-cfowler <ciera.fowler@ori.co>
2024-11-26 10:19:30 +0000

    reverting changes I was testing

--- tensorrt/llama/convert_checkpoint.py
+++ tensorrt/llama/convert_checkpoint.py
@@ -13,10 +13,8 @@ from tensorrt_llm.layers import MoeConfig
 from tensorrt_llm.logger import logger
 from tensorrt_llm.mapping import Mapping
 from tensorrt_llm.models import LLaMAForCausalLM
-from tensorrt_llm.models.convert_utils import infer_dtype
 from tensorrt_llm.models.modeling_utils import QuantConfig
 from tensorrt_llm.quantization import QuantAlgo
-from torch import save
 
 
 def parse_arguments():
@@ -373,6 +371,7 @@ def args_to_build_options(args):
 
 def convert_and_save_hf(args):
     model_dir = args.model_dir
+    load_by_shard = args.load_by_shard
     world_size = args.tp_size * args.pp_size
     # Need to convert the cli args to the kay-value pairs and override them in the generate config dict.
     # Ideally these fields will be moved out of the config and pass them into build API, keep them here for compatibility purpose for now,
@@ -389,26 +388,59 @@ def convert_and_save_hf(args):
         # llava_llama needs its own defined config.
         logger.warning("AutoConfig cannot load the huggingface config.")
 
-    mapping = Mapping(
-        world_size=world_size,
-        tp_size=args.tp_size,
-        pp_size=args.pp_size,
-        moe_tp_size=args.moe_tp_size,
-        moe_ep_size=args.moe_ep_size,
-    )
-    # TODO: support moe quantization for tp + ep
-    LLaMAForCausalLM.quantize(
-        args.model_dir,
-        args.output_dir,
-        dtype=args.dtype,
-        mapping=mapping,
-        quant_config=quant_config,
-        device="cpu" if args.load_model_on_cpu else "cuda",
-        calib_dataset=args.calib_dataset,
-        calib_batches=args.calib_size,
-        calib_max_seq_length=args.calib_max_seq_length,
-        **override_fields,
-    )
+    if args.smoothquant is not None or args.int8_kv_cache:
+        assert (
+            not args.load_by_shard
+        ), "When using quantization, TRT-LLM needs to load the whole HF model, thus load by shard not supported"
+        mapping = Mapping(
+            world_size=world_size,
+            tp_size=args.tp_size,
+            pp_size=args.pp_size,
+            moe_tp_size=args.moe_tp_size,
+            moe_ep_size=args.moe_ep_size,
+        )
+        # TODO: support moe quantization for tp + ep
+        LLaMAForCausalLM.quantize(
+            args.model_dir,
+            args.output_dir,
+            dtype=args.dtype,
+            mapping=mapping,
+            quant_config=quant_config,
+            device="cpu" if args.load_model_on_cpu else "cuda",
+            calib_dataset=args.calib_dataset,
+            calib_batches=args.calib_size,
+            calib_max_seq_length=args.calib_max_seq_length,
+            **override_fields,
+        )
+    else:
+        # When not loading by shard, preload one complete model and then slice per rank weights from this
+        # in order to save the disk reloading time
+        def convert_and_save_rank(args, rank):
+            mapping = Mapping(
+                world_size=world_size,
+                rank=rank,
+                tp_size=args.tp_size,
+                pp_size=args.pp_size,
+                moe_tp_size=args.moe_tp_size,
+                moe_ep_size=args.moe_ep_size,
+            )
+            tik = time.time()
+            llama = LLaMAForCausalLM.from_hugging_face(
+                model_dir,
+                args.dtype,
+                mapping=mapping,
+                quant_config=quant_config,
+                load_by_shard=load_by_shard,
+                **override_fields,
+            )
+            print(f"Total time of reading and converting: {time.time() - tik:.3f} s")
+            tik = time.time()
+            llama.save_checkpoint(args.output_dir, save_config=(rank == 0))
+            del llama
+            print(f"Total time of saving checkpoint: {time.time() - tik:.3f} s")
+        execute(args.workers, [convert_and_save_rank] * world_size, args)
+        release_gc()
+
 
 def execute(workers, func, args):
     if workers == 1:
