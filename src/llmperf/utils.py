de07c83772e718a1b6f38c3989a848e8c1e69a95
ori-cfowler <ciera.fowler@ori.co>
2024-12-09 13:32:36 -0500

    add parse common args function in utils

--- src/llmperf/utils.py
+++ src/llmperf/utils.py
@@ -264,3 +264,248 @@ class TensorRT:
             encoder_output_lengths,
             decoder_input_ids,
         )
+
+    @staticmethod
+    def add_common_args(parser):
+        # sampling arguments
+        parser.add_argument('--num_beams',
+                            type=int,
+                            help="Use beam search if num_beams > 1",
+                            default=1)
+        parser.add_argument('--num_return_sequences',
+                            type=int,
+                            help="Number of sequences to generate for each input.",
+                            default=None)
+        parser.add_argument('--temperature', type=float, default=1.0)
+        parser.add_argument('--top_k', type=int, default=1)
+        parser.add_argument('--top_p', type=float, default=0.0)
+        parser.add_argument('--length_penalty', type=float, default=1.0)
+        parser.add_argument('--repetition_penalty', type=float, default=1.0)
+        parser.add_argument('--presence_penalty', type=float, default=0.0)
+        parser.add_argument('--frequency_penalty', type=float, default=0.0)
+        parser.add_argument('--beam_search_diversity_rate', type=float, default=0.0)
+        parser.add_argument('--random_seed', type=int, default=0)
+        parser.add_argument('--early_stopping',
+                            type=int,
+                            help='Use early stopping if num_beams > 1, '
+                                 '1 for early-stopping, 0 for non-early-stopping'
+                                 'other values for stopping by length',
+                            default=1)
+        parser.add_argument(
+            '--end_id',
+            default=None,
+            type=int,
+            help="Override tokenizer end_id to stop on given end_id token.")
+        parser.add_argument(
+            '--stop_words',
+            default=None,
+            type=str,
+            nargs="+",
+            action='append',
+            help=
+            'Set stop words for a batch. Successive invocations of --stop_words set stop words for other batches.'
+            '    E.g.: --stop_words " London" " chef" --stop_words "eventually became" "was not"',
+        )
+        parser.add_argument(
+            '--bad_words',
+            default=None,
+            type=str,
+            nargs="+",
+            action='append',
+            help=
+            'Set bad words for a batch. Successive invocations of --bad_words set bad words for other batches.'
+            '    E.g.: --bad_words " London" " chef" --bad_words "eventually became" "was not"',
+        )
+        parser.add_argument('--no_repeat_ngram_size', type=int, default=None)
+
+        # common runtime arguments
+        parser.add_argument('--sink_token_length',
+                            type=int,
+                            default=None,
+                            help='The sink token length.')
+        parser.add_argument(
+            '--max_attention_window_size',
+            type=int,
+            default=None,
+            nargs="+",
+            help=
+            'The attention window size that controls the sliding window attention / cyclic kv cache behavior'
+        )
+        parser.add_argument(
+            '--multi_block_mode',
+            type=lambda s: s.lower() in
+                           ("yes", "true", "t", "1"
+                            ),  # custom boolean function to convert input string to boolean
+            default=True,
+            help=
+            "Distribute the work across multiple CUDA thread-blocks on the GPU for masked MHA kernel."
+        )
+        parser.add_argument('--enable_context_fmha_fp32_acc',
+                            action='store_true',
+                            help="Enable FMHA runner FP32 accumulation.")
+        parser.add_argument('--cuda_graph_mode',
+                            action='store_true',
+                            help="Enable cuda graphs in the inference.")
+        parser.add_argument(
+            '--log_level',
+            type=str,
+            choices=['verbose', 'info', 'warning', 'error', 'internal_error'],
+            default='info')
+        parser.add_argument(
+            '--no_prompt_template',
+            dest='use_prompt_template',
+            default=True,
+            action='store_false',
+            help=
+            "Whether or not to use default prompt template to wrap the input text.")
+        parser.add_argument('--use_py_session',
+                            default=False,
+                            action='store_true',
+                            help="Whether or not to use Python runtime session")
+        parser.add_argument('--debug_mode',
+                            default=False,
+                            action='store_true',
+                            help="Whether or not to turn on the debug mode")
+        parser.add_argument('--streaming', default=False, action='store_true')
+        parser.add_argument('--streaming_interval',
+                            type=int,
+                            help="How often to return tokens when streaming.",
+                            default=5)
+        parser.add_argument(
+            '--prompt_table_path',
+            type=str,
+            help="Path to .npy file, exported by nemo_prompt_convert.py")
+        parser.add_argument(
+            '--prompt_tasks',
+            help="Comma-separated list of tasks for prompt tuning, e.g., 0,3,1,0")
+        parser.add_argument('--lora_dir',
+                            type=str,
+                            default=None,
+                            nargs="+",
+                            help="The directory of LoRA weights")
+        parser.add_argument('--lora_ckpt_source',
+                            type=str,
+                            default="hf",
+                            choices=["hf", "nemo"],
+                            help="The source of lora checkpoint.")
+        parser.add_argument(
+            '--lora_task_uids',
+            type=str,
+            default=None,
+            nargs="+",
+            help="The list of LoRA task uids; use -1 to disable the LoRA module")
+        parser.add_argument(
+            '--num_prepend_vtokens',
+            nargs="+",
+            type=int,
+            help="Number of (default) virtual tokens to prepend to each sentence."
+                 " For example, '--num_prepend_vtokens=10' will prepend the tokens"
+                 " [vocab_size, vocab_size + 1, ..., vocab_size + 9] to the sentence.")
+        parser.add_argument(
+            '--draft_target_model_config',
+            type=str,
+            default=None,
+            help=
+            "Configuration of Draft-Target-Model decoding, see `examples/draft_target_model/README.md` for more information."
+            "   E.g.: [4, [0], [1], False] for [draft_len, draft_model_device_list, target_model_device_list, use_logits]."
+        )
+        parser.add_argument(
+            '--medusa_choices',
+            type=str,
+            default=None,
+            help="Configuration of Medusa decoding."
+                 "   E.g.: [[0, 0, 0, 0], [0, 1, 0], [1, 0], [1, 1]] for 9 medusa tokens."
+        )
+        parser.add_argument(
+            '--lookahead_config',
+            type=str,
+            default=None,
+            help="Configuration of executor and request lookahead decoding."
+                 "   E.g.: [5, 6, 7] for [max_window_size, max_ngram_size, max_verification_set_size]."
+        )
+        # model arguments
+        parser.add_argument('--engine_dir', type=str, default='engine_outputs')
+        parser.add_argument(
+            '--tokenizer_type',
+            help=
+            'Specify that argument when providing a .model file as the tokenizer_dir. '
+            'It allows AutoTokenizer to instantiate the correct tokenizer type.')
+        parser.add_argument('--vocab_file',
+                            help="Used for sentencepiece tokenizers")
+        parser.add_argument('--no_add_special_tokens',
+                            dest='add_special_tokens',
+                            default=True,
+                            action='store_false',
+                            help="Whether or not to add special tokens")
+        parser.add_argument('--hf_model_dir', '--model_dir', type=str, default=None)
+        parser.add_argument(
+            '--tokenizer_dir',
+            default=None,
+            help='tokenizer path; defaults to hf_model_dir if left unspecified')
+
+        # memory argument
+        parser.add_argument(
+            '--gpu_weights_percent',
+            default=1,
+            type=float,
+            help=
+            'Specify the percentage of weights that reside on GPU instead of CPU and streaming load during runtime.',
+        )
+        parser.add_argument(
+            '--max_tokens_in_paged_kv_cache',
+            default=None,
+            type=int,
+            help=
+            'Specify the maximum number of tokens in a kv cache page (only available with cpp session).',
+        )
+        parser.add_argument(
+            '--kv_cache_enable_block_reuse',
+            action='store_true',
+            help=
+            'Enables block reuse in kv cache (only available with cpp session).',
+        )
+        parser.add_argument(
+            '--kv_cache_free_gpu_memory_fraction',
+            default=0.9,
+            type=float,
+            help='Specify the free gpu memory fraction.',
+        )
+        parser.add_argument(
+            '--cross_kv_cache_fraction',
+            default=0.5,
+            type=float,
+            help=
+            'Specify the kv cache fraction reserved for cross attention. Only applicable for encoder-decoder models. By default 0.5 for self and 0.5 for cross.',
+        )
+        parser.add_argument(
+            '--enable_chunked_context',
+            action='store_true',
+            help='Enables chunked context (only available with cpp session).',
+        )
+
+        # hf model argument (if use hf model)
+        parser.add_argument(
+            '--hf_data_type',
+            '--data_type',
+            type=str,
+            choices=['fp32', 'fp16', 'bf16', 'float32', 'float16', 'bfloat16'],
+            default='fp16',
+            help="The data type for hf model.")
+        parser.add_argument(
+            '--hf_device_map_auto',
+            action='store_true',
+            help="Use device map 'auto' to load a pretrained HF model. This may "
+                 "help to test a large model that cannot fit into a singlue GPU.")
+
+        parser.add_argument(
+            "--return_all_generated_tokens",
+            default=False,
+            action="store_true",
+            help="This option changes the token output only for streaming. "
+                 "If not specified, return only generated tokens at each step. "
+                 "If specified, return the full beams/outputs at each step. "
+                 "It is automatically enabled for num_beams>1 (only available with cpp session). "
+                 "WARNING: using this option may increase network usage significantly (quadratically w.r.t output length)."
+        )
+
+        return parser
